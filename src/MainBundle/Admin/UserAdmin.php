cc356d6857c8ca0e47823467aa4305aa2c17b995
MaximStrutinskiy <max.strutinskiy@gmail.com>
2017-01-21 21:39:32 +0200

    show for admin - image users

--- src/MainBundle/Admin/UserAdmin.php
+++ src/MainBundle/Admin/UserAdmin.php
@@ -8,6 +8,8 @@ use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Form\FormMapper;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use MainBundle\Entity\User as User;
 
@@ -38,6 +40,19 @@ class UserAdmin extends Admin
                         'User' => 'ROLE_USER',
                     ),
                 )
+            )
+            ->add('name', TextType::class)
+            ->add('soname', TextType::class)
+            ->add('age', IntegerType::class)
+            ->add('city', TextType::class)
+            ->add(
+                'Img',
+                FileType::class,
+                array(
+                    'label' => 'Upload User img (PNG file)',
+                    'data_class' => null,
+                    'required' => false,
+                )
             );
     }
 
@@ -47,16 +62,31 @@ class UserAdmin extends Admin
             ->add('username')
             ->add('email')
             ->add('enabled')
-            ->add('roles');
+            ->add('roles')
+            ->add('name')
+            ->add('soname')
+            ->add('age')
+            ->add('city');
     }
 
     protected function configureListFields(ListMapper $listMapper)
     {
         $listMapper
-            ->addIdentifier('username')
-            ->addIdentifier('email')
+            ->add('username')
+            ->add('email')
             ->add('enabled')
             ->add('roles')
+            ->add('name')
+            ->add('soname')
+            ->add('age')
+            ->add('city')
+            ->add(
+                'img',
+                null,
+                array(
+                    'template' => 'MainBundle:SonataAdminBundle/AdminPanel:user_list_fields.html.twig',
+                )
+            )
             ->add(
                 '_action',
                 'actions',
@@ -67,6 +97,37 @@ class UserAdmin extends Admin
                     ],
                 ]
             );
+    }
+
+    /**
+     * @post User $object
+     */
+    public function preUpdate($object)
+    {
+        $this->processImage($object);
+    }
+
+    public function prePersist($object)
+    {
+        $this->processImage($object);
+    }
+
+    private function processImage($object)
+    {
+        $img = $object->getImg();
+        if ($img !== null) {
+            // Generate a unique name for the file before saving it
+            $fileName = md5(uniqid()).'.'.$img->getExtension();
+
+            // Move the file to the directory where brochures are stored
+            $img->move(
+                $this->getConfigurationPool()->getContainer()->getParameter('user_images'),
+                $fileName
+            );
+            // Update the 'img' property to store the img file name
+            // instead of its contents
+            $object->setImg($fileName);
+        }
     }
 
     public function toString($object)
