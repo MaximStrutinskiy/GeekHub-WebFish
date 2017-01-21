af6ad14d80b2dcf6f4031afc36baf66c5b3e938f
MaximStrutinskiy <max.strutinskiy@gmail.com>
2017-01-21 20:33:46 +0200

    admin panel - add Role for user

--- src/MainBundle/Admin/UserAdmin.php
+++ src/MainBundle/Admin/UserAdmin.php
@@ -6,6 +6,8 @@ use Sonata\AdminBundle\Admin\AbstractAdmin as Admin;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Form\FormMapper;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use MainBundle\Entity\User as User;
 
@@ -15,7 +17,23 @@ class UserAdmin extends Admin
     {
         $formMapper
             ->add('username', TextType::class)
-            ->add('username', TextType::class)
+            ->add('email', EmailType::class)
+            ->add('enabled', ChoiceType::class, array(
+                'multiple' => false,
+                'choice_label' => 'Role',
+                'choices'  => array(
+                    'Admin' => 'a:1:{i:0;s:16:"ROLE_SUPER_ADMIN";}',
+                    'User' => 'a:0:{}',
+//                    'Moderator' => array('[0 => ROLE_MODERATOR]', '[1 => ROLE_USER]'),
+                ),
+            ))
+            ->add('roles', ChoiceType::class, array(
+                'choices'  => array(
+                    'Yes' => true,
+                    'No' => false,
+                ),
+            ))
+
         ;
     }
 
@@ -23,6 +41,9 @@ class UserAdmin extends Admin
     {
         $datagridMapper
             ->add('username')
+            ->add('email')
+            ->add('enabled')
+            ->add('roles')
         ;
     }
 
@@ -30,6 +51,16 @@ class UserAdmin extends Admin
     {
         $listMapper
             ->addIdentifier('username')
+            ->addIdentifier('email')
+            ->add('enabled')
+            ->add('roles')
+
+            ->add('_action', 'actions', [
+                'actions' => [
+                    'edit' => [],
+                    'delete' => [],
+                ]
+            ])
         ;
     }
 
