31ff5b3cf2279c143937f149323d8dc82f899721
MaximStrutinskiy <max.strutinskiy@gmail.com>
2017-01-26 16:33:56 +0200

    style fix

--- web/assets/assets/js/main.min.js
+++ web/assets/assets/js/main.min.js
@@ -1,25 +1,23 @@
 $(function () {
-	$(document).ready(function () {
+  $(document).ready(function () {
 
+    //drop down menu
+    var dropMenuId = document.getElementById('mmenu-user');
+    if (dropMenuId) {
+      function dropDownMenu() {
+        // var dropButton = $('#mmenu-user');
+        var dropContent = $('#mmenu-user > #mmenu-user-content');
+        $('#mmenu-user').hover(
+          function () {
+            dropContent.addClass('active').stop(true, true).fadeIn(300);
+          },
+          function () {
+            dropContent.removeClass('active').stop(true, true).fadeOut();
+          }
+        )
+      }
+      dropDownMenu();
+    }
 
-		//drop down menu
-		var dropMenuId = document.getElementById('mmenu-user');
-		if (dropMenuId) {
-			function dropDownMenu() {
-				// var dropButton = $('#mmenu-user');
-				var dropContent = $('#mmenu-user > #mmenu-user-content');
-				$('#mmenu-user').hover(
-					function () {
-						dropContent.addClass('active').stop(true, true).fadeIn(300);
-					},
-					function () {
-						dropContent.removeClass('active').stop(true, true).fadeOut();
-					}
-				)
-			}
-
-			dropDownMenu();
-		}
-
-	});
+  });
 });
