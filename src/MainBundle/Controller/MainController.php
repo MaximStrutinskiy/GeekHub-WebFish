a5f582487c8970fc59ee5e3b7b294156b827de20
MaximStrutinskiy <max.strutinskiy@gmail.com>
2017-01-30 11:07:33 +0200

    fixture for post - fix

--- src/MainBundle/Controller/MainController.php
+++ src/MainBundle/Controller/MainController.php
@@ -9,23 +9,35 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
  *
  * @package MainBundle\Controller
  */
-class MainController extends Controller
-{
+class MainController extends Controller {
 
-    public function indexAction()
-    {
-        return $this->render('MainBundle::base.html.twig');
-    }
+  /**
+   * @return \Symfony\Component\HttpFoundation\Response
+   */
+  public function indexAction() {
+    return $this->render('MainBundle::base.html.twig');
+  }
 
-    public function blogAction()
-    {
-        //add breadcrumbs
-        $breadcrumbs = $this
-            ->get('white_october_breadcrumbs')
-            ->addItem('Home', $this->get('router')->generate('home'))
-            ->addItem('Blog')
-        ;
+  /**
+   * @return \Symfony\Component\HttpFoundation\Response
+   */
+  public function blogAction() {
+    //add breadcrumbs
+    $breadcrumbs = $this
+      ->get('white_october_breadcrumbs')
+      ->addItem('Home', $this->get('router')->generate('home'))
+      ->addItem('Blog');
 
-        return $this->render('MainBundle:Page:_blog.html.twig');
-    }
+    return $this->render('MainBundle:Page:_blog.html.twig');
+  }
+
+  /**
+   * For Fixture bundle
+   */
+  public function selectOneRandomCategory() {
+    $em = $this
+      ->getDoctrine()
+      ->getRepository('MainBundle:Category')
+      ->findRandomCategoryQuery();
+  }
 }
--- src/MainBundle/DataFixtures/ORM/LoadPost.php
+++ src/MainBundle/DataFixtures/ORM/LoadPost.php
@@ -3,67 +3,89 @@ namespace MainBundle\DataFixtures\ORM;
 
 use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use MainBundle\Controller\MainController;
 use MainBundle\Entity\Post as Post;
 use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 
 use Faker\Factory as Faker;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
-class LoadPostData extends AbstractFixture implements OrderedFixtureInterface
-{
+/**
+ * Class LoadPostData
+ *
+ * @package MainBundle\DataFixtures\ORM
+ */
+class LoadPostData extends AbstractFixture implements OrderedFixtureInterface {
 
-    public function load(ObjectManager $manager)
-    {
+  /** @var  ContainerInterface */
+  private $container;
 
-        //all category in LoadCategoryData.php
-        //all tags in LoadTagData.php
-        //all user in LoadUserData.php
+  public function load(ObjectManager $manager) {
 
-        $faker = Faker::create();
+    //all category in LoadCategoryData.php
+    //all tags in LoadTagData.php
+    //all user in LoadUserData.php
 
-        $postContent = [];
+    $faker = Faker::create();
+    $postContent = [];
 
-        for ($i = 1; $i <= 20; $i++) {
-            $var = [
-                $faker->sentence($nbWords = 3, $variableNbWords = true),
-                $faker->sentence($nbWords = 8, $variableNbWords = true),
-                $faker->sentence($nbWords = 20, $variableNbWords = true),
-                $faker->sentence($nbWords = 2000, $variableNbWords = true),
-                '1',
-                $query,
-                $faker->imageUrl($width = 1200, $height = 650),
-            ];
+    $randomCategory = new MainController;
+    $randomCategory->selectOneRandomCategory();
 
-            array_push($postContent, $var);
-        }
+    for ($i = 1; $i <= 20; $i++) {
+      $var = [
+        $faker->sentence($nbWords = 3, $variableNbWords = TRUE),
+        $faker->sentence($nbWords = 8, $variableNbWords = TRUE),
+        $faker->sentence($nbWords = 20, $variableNbWords = TRUE),
+        $faker->sentence($nbWords = 2000, $variableNbWords = TRUE),
+        '1',
+        $randomCategory,
+        $faker->imageUrl($width = 1200, $height = 650),
+        ['Flowers', 'Architecture'],
+      ];
+
+      array_push($postContent, $var);
+    }
 
 
-        foreach ($postContent as list($shortTitle, $longTitle, $shortDescription, $longDescription, $category, $postStatus, $image)) {
-            $post = new Post();
-            $post->setShortTitle($shortTitle);
-            $post->setLongTitle($longTitle);
-            $post->setShortDescriptions($shortDescription);
-            $post->setLongDescriptions($longDescription);
+    foreach ($postContent as list($shortTitle, $longTitle, $shortDescription, $longDescription, $user, $postStatus, $category, $setTags, $image)) {
+      $post = new Post();
+      $post->setShortTitle($shortTitle);
+      $post->setLongTitle($longTitle);
+      $post->setShortDescriptions($shortDescription);
+      $post->setLongDescriptions($longDescription);
 
-//            $post->setUser($user);
+            $post->setUser($user);
 
-            $post->setPostStatus($postStatus);
+      $post->setPostStatus($postStatus);
 
             $post->setCategory($this->getReference($category));
 
-//            /**@var Post $setTags */
-//            foreach ($setTags as $tagArray) {
-//                $post->addTag($this->getReference($tagArray));
-//            }
+            /**@var Post $setTags */
+            foreach ($setTags as $tagArray) {
+                $post->addTag($this->getReference($tagArray));
+            }
 
-            $post->setPostImg($image);
-            $post->setPostDate(new \DateTime);
-            $manager->persist($post);
-            $manager->flush();
-        }
+      $post->setPostImg($image);
+      $post->setPostDate(new \DateTime);
+      $manager->persist($post);
+      $manager->flush();
     }
+  }
 
-    public function getOrder()
-    {
-        return 4;
-    }
+  public function getOrder() {
+    return 4;
+  }
+
+
+  /**
+   * Sets the Container. Need it to create new User from fos_user.user_manager
+   * service
+   *
+   * @param ContainerInterface|null $container A ContainerInterface instance or
+   *   null
+   */
+  public function setContainer(ContainerInterface $container = NULL) {
+    $this->container = $container;
+  }
 }
