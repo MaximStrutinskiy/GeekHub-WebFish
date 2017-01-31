79744b0cc0485ca30d91eb4f4ada1ba10f49a303
MaximStrutinskiy <max.strutinskiy@gmail.com>
2017-01-31 21:02:21 +0200

    add form for comment

---
+++ src/MainBundle/Forms/FormCommentType.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace MainBundle\Forms;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ResetType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+/**
+ * Class FormInfo
+ * Use BlogBundle\Entity\Info
+ * @package BlogBundle\Forms
+ */
+class FormCommentType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add(
+                'commentText',
+                TextareaType::class,
+                array(
+                    'attr' => array('placeholder' => 'Enter your commit'),
+                )
+            )
+            ->add(
+                'send',
+                SubmitType::class
+            )
+            ->add('clean', ResetType::class);
+    }
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(
+            [
+                'data_class' => 'MainBundle\Entity\Comment',
+            ]
+        );
+    }
+}
