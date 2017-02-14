b926ece7970a29c79b3fb1e6ecd615e96004ca18
MaximStrutinskiy <max.strutinskiy@gmail.com>
2017-02-14 17:48:45 +0200

    voter

--- src/MainBundle/Security/CommentVoter.php
+++ src/MainBundle/Security/CommentVoter.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace MainBundle\Security;
 
 use MainBundle\Entity\Comment;
@@ -6,25 +7,22 @@ use MainBundle\Entity\User;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Authorization\Voter\Voter;
 
+/**
+ * Class CommentVoter
+ *
+ * @package MainBundle\Security
+ */
 class CommentVoter extends Voter {
-// these strings are just invented: you can use anything
+
   const VIEW = 'view';
   const EDIT = 'edit';
   const DELETE = 'delete';
 
-  /**
-   * @param string $attribute
-   * @param mixed $subject
-   *
-   * @return bool
-   */
   protected function supports($attribute, $subject) {
-// if the attribute isn't one we support, return false
     if (!in_array($attribute, array(self::VIEW, self::EDIT, self::DELETE))) {
       return FALSE;
     }
 
-// only vote on Post objects inside this voter
     if (!$subject instanceof Comment) {
       return FALSE;
     }
@@ -32,18 +30,10 @@ class CommentVoter extends Voter {
     return TRUE;
   }
 
-  /**
-   * @param string $attribute
-   * @param mixed $subject
-   * @param \Symfony\Component\Security\Core\Authentication\Token\TokenInterface $token
-   *
-   * @return bool
-   */
   protected function voteOnAttribute($attribute, $subject, TokenInterface $token) {
     $user = $token->getUser();
 
     if (!$user instanceof User) {
-// the user must be logged in; if not, deny access
       return FALSE;
     }
 
@@ -63,44 +53,18 @@ class CommentVoter extends Voter {
     throw new \LogicException('This code should not be reached!');
   }
 
-  /**
-   * @param \MainBundle\Entity\Comment $comment
-   * @param \MainBundle\Entity\User $user
-   *
-   * @return mixed
-   */
   private function canView(Comment $comment, User $user) {
     return $this->currentUserRights($comment, $user);
   }
 
-  /**
-   * @param \MainBundle\Entity\Comment $comment
-   * @param \MainBundle\Entity\User $user
-   *
-   * @return mixed
-   */
   private function canEdit(Comment $comment, User $user) {
     return $this->currentUserRights($comment, $user);
   }
 
-  /**
-   * @param \MainBundle\Entity\Comment $comment
-   * @param \MainBundle\Entity\User $user
-   *
-   * @return mixed
-   */
   private function canDelete(Comment $comment, User $user) {
     return $this->currentUserRights($comment, $user);
   }
 
-  /**
-   * Check if current user have ROLE_ADMIN or ROLE_MODERATOR, if not - compare current user id with author of comment, if true - give CRUD permissions.
-   *
-   * @param Comment $comment
-   * @param User $user
-   *
-   * @return bool
-   */
   private function currentUserRights(Comment $comment, User $user) {
     foreach ($user->getRoles() as $role) {
       if ('ROLE_SUPER_ADMIN' === $role || 'ROLE_MODERATOR' === $role) {
