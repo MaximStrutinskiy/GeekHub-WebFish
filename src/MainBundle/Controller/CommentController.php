9f07e3546b21de5bf8d89119525813d3e68ecda3
MaximStrutinskiy <max.strutinskiy@gmail.com>
2017-02-18 21:14:07 +0200

    functionality comment can add to 1 lvl

--- src/MainBundle/Controller/CommentController.php
+++ src/MainBundle/Controller/CommentController.php
@@ -7,132 +7,138 @@ use MainBundle\Forms\FormCommentType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
-class CommentController extends Controller {
-  // Check blog like action !! code review
-  public function addCommentForCommentAction(Request $request, Post $post, $id, $commentId) {
-    $em = $this->getDoctrine()->getManager();
-    $postRepository = $em->getRepository("MainBundle:Post");
-    $post = $postRepository->find($id);
-    $postCountComment = $postRepository->findCountPostsWithCommentResult($id);
+class CommentController extends Controller
+{
+    // Check blog like action !! code review
+    public function addCommentForCommentAction(Request $request, Post $post, $id, $commentId)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $postRepository = $em->getRepository("MainBundle:Post");
+        $post = $postRepository->find($id);
+        $postCountComment = $postRepository->findCountPostsWithCommentResult($id);
 
-    $commentRepository = $em->getRepository("MainBundle:Comment");
-    $commentPost = $commentRepository->findAllComments($id);
+        $commentRepository = $em->getRepository("MainBundle:Comment");
+        $commentPost = $commentRepository->findAllComments($id);
 
-    $childComment = new Comment();
-    $parentComment = $this->getDoctrine()
-                    ->getRepository('MainBundle:Comment')
-                    ->find($commentId);
+        $childComment = new Comment();
+        $parentComment = $this->getDoctrine()
+            ->getRepository('MainBundle:Comment')
+            ->find($commentId);
 
-    $childComment
-      ->setPost($post)
-      ->setUser($this->getUser())
-      ->setParent($parentComment);
+        if ($childComment->getParent() == null){
+            $childComment
+                ->setPost($post)
+                ->setUser($this->getUser())
+                ->setParent($parentComment);
 
-    $form = $this->createForm(FormCommentType::class, $childComment);
-    $form->handleRequest($request);
+            $form = $this->createForm(FormCommentType::class, $childComment);
+            $form->handleRequest($request);
 
-    if ($form->isSubmitted() && $form->isValid()) {
-      $comment = $form->getData();
-      $em = $this->getDoctrine()->getManager();
-      $em->persist($comment);
-      $em->flush();
+            if ($form->isSubmitted() && $form->isValid()) {
+                $comment = $form->getData();
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($comment);
+                $em->flush();
 
-      return $this->redirect(
-        $this->generateUrl(
-          'blog-post',
-          [
-            'id' => $post->getId(),
-            'shortTitle' => $post->getShortTitle(),
-          ]
-        )
-      );
+                return $this->redirect(
+                    $this->generateUrl(
+                        'blog-post',
+                        [
+                            'id' => $post->getId(),
+                            'shortTitle' => $post->getShortTitle(),
+                        ]
+                    )
+                );
+            }
+
+            return $this->render(
+                'MainBundle:Page:_internal_blog.html.twig',
+                [
+                    'post' => $post,
+                    'show_comment' => $commentPost,
+                    'form_comment' => $form->createView(),
+                    'count_comment' => $postCountComment->getResult(),
+                ]
+            );
+        }
+
     }
 
-    return $this->render(
-      'MainBundle:Page:_internal_blog.html.twig',
-      [
-        'post' => $post,
-        'show_comment' => $commentPost,
-        'form_comment' => $form->createView(),
-        'count_comment' => $postCountComment->getResult(),
-      ]
-    );
-  }
+    // Check blog like action !! code review
+    public function editAction(Request $request, Post $post, $id, $commentId)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $postRepository = $em->getRepository("MainBundle:Post");
+        $post = $postRepository->find($id);
+        $postCountComment = $postRepository->findCountPostsWithCommentResult($id);
 
-  // Check blog like action !! code review
-  public function editAction(Request $request, Post $post, $id, $commentId) {
-    $em = $this->getDoctrine()->getManager();
-    $postRepository = $em->getRepository("MainBundle:Post");
-    $post = $postRepository->find($id);
-    $postCountComment = $postRepository->findCountPostsWithCommentResult($id);
+        $commentRepository = $em->getRepository("MainBundle:Comment");
+        $commentPost = $commentRepository->findAllComments($id);
 
-    $commentRepository = $em->getRepository("MainBundle:Comment");
-    $commentPost = $commentRepository->findAllComments($id);
+        if (!$commentId) {
+            $comment = new Comment();
+        } else {
+            $comment = $this->getDoctrine()
+                ->getRepository('MainBundle:Comment')
+                ->find($commentId);
+        }
+        $comment->setPost($post);
+        $form = $this->createForm(FormCommentType::class, $comment);
+        $form->handleRequest($request);
 
-    if (!$commentId) {
-      $comment = new Comment();
+        $this->denyAccessUnlessGranted('edit_comment', $comment);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $comment = $form->getData();
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($comment);
+            $em->flush();
+
+            return $this->redirect(
+                $this->generateUrl(
+                    'blog-post',
+                    [
+                        'id' => $post->getId(),
+                        'shortTitle' => $post->getShortTitle(),
+                    ]
+                )
+            );
+        }
+
+        return $this->render(
+            'MainBundle:Page:_internal_blog.html.twig',
+            [
+                'post' => $post,
+                'show_comment' => $commentPost,
+                'form_comment' => $form->createView(),
+                'count_comment' => $postCountComment->getResult(),
+            ]
+        );
     }
-    else {
-      $comment = $this->getDoctrine()
-                      ->getRepository('MainBundle:Comment')
-                      ->find($commentId);
+
+    public function deleteAction(Request $request, $id, $commentId)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $postRepository = $em->getRepository('MainBundle:Post');
+        $commentRepository = $em->getRepository('MainBundle:Comment');
+
+        $post = $postRepository->find($id);
+        $comment = $commentRepository->find($commentId);
+
+        $this->denyAccessUnlessGranted('delete_comment', $comment);
+
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($comment);
+        $em->flush();
+
+        return $this->redirect(
+            $this->generateUrl(
+                'blog-post',
+                [
+                    'id' => $post->getId(),
+                    'shortTitle' => $post->getShortTitle(),
+                ]
+            )
+        );
     }
-    $comment->setPost($post);
-    $form = $this->createForm(FormCommentType::class, $comment);
-    $form->handleRequest($request);
-
-    $this->denyAccessUnlessGranted('edit_comment', $comment);
-
-    if ($form->isSubmitted() && $form->isValid()) {
-      $comment = $form->getData();
-      $em = $this->getDoctrine()->getManager();
-      $em->persist($comment);
-      $em->flush();
-
-      return $this->redirect(
-        $this->generateUrl(
-          'blog-post',
-          [
-            'id' => $post->getId(),
-            'shortTitle' => $post->getShortTitle(),
-          ]
-        )
-      );
-    }
-
-    return $this->render(
-      'MainBundle:Page:_internal_blog.html.twig',
-      [
-        'post' => $post,
-        'show_comment' => $commentPost,
-        'form_comment' => $form->createView(),
-        'count_comment' => $postCountComment->getResult(),
-      ]
-    );
-  }
-
-  public function deleteAction(Request $request, $id, $commentId) {
-    $em = $this->getDoctrine()->getManager();
-    $postRepository = $em->getRepository('MainBundle:Post');
-    $commentRepository = $em->getRepository('MainBundle:Comment');
-
-    $post = $postRepository->find($id);
-    $comment = $commentRepository->find($commentId);
-
-    $this->denyAccessUnlessGranted('delete_comment', $comment);
-
-    $em = $this->getDoctrine()->getManager();
-    $em->remove($comment);
-    $em->flush();
-
-    return $this->redirect(
-      $this->generateUrl(
-        'blog-post',
-        [
-          'id' => $post->getId(),
-          'shortTitle' => $post->getShortTitle(),
-        ]
-      )
-    );
-  }
 }
--- src/MainBundle/Repository/PostRepository.php
+++ src/MainBundle/Repository/PostRepository.php
@@ -12,98 +12,106 @@ use MainBundle\Entity\Tag;
  *
  * @package MainBundle\Repository
  */
-class PostRepository extends EntityRepository {
-  public function findPostsQuery() {
-    $qb = $this->createQueryBuilder('p');
-    $qb
-      ->where('p.postStatus = :postStatus')
-      ->orderBy('p.id', 'DESC')
-      ->setParameter('postStatus', TRUE);
+class PostRepository extends EntityRepository
+{
+    public function findPostsQuery()
+    {
+        $qb = $this->createQueryBuilder('p');
+        $qb
+            ->where('p.postStatus = :postStatus')
+            ->orderBy('p.id', 'DESC')
+            ->setParameter('postStatus', true);
 
-    return $qb->getQuery();
-  }
+        return $qb->getQuery();
+    }
 
-  public function findPostsWithCountCommentQuery() {
-    $qb = $this->createQueryBuilder('p');
-    $qb
-      ->select('p', 'p, COUNT (c) AS count_post_comments')
-      ->where('p.postStatus = :postStatus')
-      ->leftJoin('p.postComment', 'c')
-      ->groupBy('p.id')
-      ->orderBy('p.id', 'DESC')
-      ->setParameter('postStatus', TRUE);
+    public function findPostsWithCountCommentQuery()
+    {
+        $qb = $this->createQueryBuilder('p');
+        $qb
+            ->select('p', 'p, COUNT (c) AS count_post_comments')
+            ->where('p.postStatus = :postStatus')
+            ->leftJoin('p.postComment', 'c')
+            ->groupBy('p.id')
+            ->orderBy('p.id', 'DESC')
+            ->setParameter('postStatus', true);
 
-    return $qb->getQuery();
-  }
+        return $qb->getQuery();
+    }
 
-  public function findPostsWithCountLikeQuery() {
-    $qb = $this->createQueryBuilder('p');
-    $qb
-      ->select('p', 'p, COUNT (l) AS count_post_like')
-      ->where('p.postStatus = :postStatus')
-      ->leftJoin('p.postLike', 'l')
-      ->groupBy('p.id')
-      ->orderBy('p.id', 'DESC')
-      ->setParameter('postStatus', TRUE);
+    public function findPostsWithCountLikeQuery()
+    {
+        $qb = $this->createQueryBuilder('p');
+        $qb
+            ->select('p', 'p, COUNT (l) AS count_post_like')
+            ->where('p.postStatus = :postStatus')
+            ->leftJoin('p.postLike', 'l')
+            ->groupBy('p.id')
+            ->orderBy('p.id', 'DESC')
+            ->setParameter('postStatus', true);
 
-    return $qb->getQuery();
-  }
+        return $qb->getQuery();
+    }
 
-  public function findAllPostByCategoryQuery($id) {
-    $qb = $this->createQueryBuilder('p');
-    $qb
-      ->select('p', 'p, COUNT (c) AS count_post_comments')
-      ->where('p.category = :idCategory')
-      ->andWhere('p.postStatus = :postStatus')
-      ->leftJoin('p.postComment', 'c')
-      ->groupBy('p.id')
-      ->orderBy('p.id', 'DESC')
-      ->setParameter('idCategory', $id)
-      ->setParameter('postStatus', TRUE);
+    public function findAllPostByCategoryQuery($id)
+    {
+        $qb = $this->createQueryBuilder('p');
+        $qb
+            ->select('p', 'p, COUNT (c) AS count_post_comments')
+            ->where('p.category = :idCategory')
+            ->andWhere('p.postStatus = :postStatus')
+            ->leftJoin('p.postComment', 'c')
+            ->groupBy('p.id')
+            ->orderBy('p.id', 'DESC')
+            ->setParameter('idCategory', $id)
+            ->setParameter('postStatus', true);
 
-    return $qb->getQuery();
-  }
+        return $qb->getQuery();
+    }
 
-  public function findCountPostsWithCategoryResult() {
-    $qb = $this->createQueryBuilder('p');
-    $qb
-      ->select('COUNT (p) AS post_count, c.name, c.id')
-      ->where('p.postStatus = :postStatus')
-      ->innerJoin('p.category', 'c')
-      ->groupBy('c.id')
-      ->setParameter('postStatus', TRUE);
+    public function findCountPostsWithCategoryResult()
+    {
+        $qb = $this->createQueryBuilder('p');
+        $qb
+            ->select('COUNT (p) AS post_count, c.name, c.id')
+            ->where('p.postStatus = :postStatus')
+            ->innerJoin('p.category', 'c')
+            ->groupBy('c.id')
+            ->setParameter('postStatus', true);
 
-    return $qb->getQuery()->getResult();
-  }
+        return $qb->getQuery()->getResult();
+    }
 
-  public function findCountPostsWithCommentResult($id) {
-    $qb = $this->createQueryBuilder('p');
-    $qb
-      ->select('p', 'p, COUNT (c) AS count_post_comments')
-      ->where('p.postStatus = :postStatus')
-      ->andWhere('p.id = :postId')
-      ->leftJoin('p.postComment', 'c')
-      ->groupBy('p.id')
-      ->orderBy('p.id', 'DESC')
-      ->setParameter('postStatus', TRUE)
-      ->setParameter('postId', $id);
+    public function findCountPostsWithCommentResult($id)
+    {
+        $qb = $this->createQueryBuilder('p');
+        $qb
+            ->select('p', 'p, COUNT (c) AS count_post_comments')
+            ->where('p.postStatus = :postStatus')
+            ->andWhere('p.id = :postId')
+            ->leftJoin('p.postComment', 'c')
+            ->groupBy('p.id')
+            ->orderBy('p.id', 'DESC')
+            ->setParameter('postStatus', true)
+            ->setParameter('postId', $id);
 
-    return $qb->getQuery();
-  }
+        return $qb->getQuery();
+    }
 
-  public function findAllPostByTagQuery(Tag $id) {
-    $qb = $this->createQueryBuilder('p');
-    $qb
-      ->select('p', 'p, COUNT (c) AS count_post_comments')
-      ->join('p.tag', 'idTag')
-      ->leftJoin('p.postComment', 'c')
-      ->groupBy('p.id')
-      ->where('idTag = :idTag')
-      ->setParameter(':idTag', $id)
-      ->andWhere('p.postStatus = :postStatus')
-      ->orderBy('p.postDate', 'DESC')
-      ->setParameter('postStatus', TRUE);
+    public function findAllPostByTagQuery(Tag $id)
+    {
+        $qb = $this->createQueryBuilder('p');
+        $qb
+            ->select('p', 'p, COUNT (c) AS count_post_comments')
+            ->join('p.tag', 'idTag')
+            ->leftJoin('p.postComment', 'c')
+            ->groupBy('p.id')
+            ->where('idTag = :idTag')
+            ->setParameter(':idTag', $id)
+            ->andWhere('p.postStatus = :postStatus')
+            ->orderBy('p.postDate', 'DESC')
+            ->setParameter('postStatus', true);
 
-    return $qb->getQuery();
-  }
+        return $qb->getQuery();
+    }
 }
