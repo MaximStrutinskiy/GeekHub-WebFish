4058de87e87847b8e61f57ad1d8aab523291aa58
MaximStrutinskiy <max.strutinskiy@gmail.com>
2017-02-21 15:34:16 +0200

    add fixture for users

--- src/MainBundle/DataFixtures/ORM/LoadUser.php
+++ src/MainBundle/DataFixtures/ORM/LoadUser.php
@@ -11,79 +11,81 @@ use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
  *
  * @package MainBundle\DataFixtures\ORM
  */
-class LoadUserData extends AbstractFixture implements OrderedFixtureInterface
-{
-    public function load(ObjectManager $manager)
-    {
+class LoadUserData extends AbstractFixture implements OrderedFixtureInterface {
+  public function load(ObjectManager $manager) {
 
-        $users = [
-            [
-                'admin',
-                'admin@admin.com',
-                'admin',
-                'TRUE',
-                ['ROLE_SUPER_ADMIN', 'ROLE_USER'],
-            ],
-            [
-                'moder1',
-                'moder1@moder.com',
-                'moder1',
-                'TRUE',
-                ['ROLE_MODERATOR', 'ROLE_USER'],
-            ],
-            [
-                'moder2',
-                'moder2@moder.com',
-                'moder2',
-                'TRUE',
-                ['ROLE_MODERATOR', 'ROLE_USER'],
-            ],
-            [
-                'user1',
-                'user1@user1.com',
-                'user1',
-                'TRUE',
-                ['ROLE_USER'],
-            ],
-            [
-                'user2',
-                'user2@user2.com',
-                'user2',
-                'TRUE',
-                ['ROLE_USER'],
-            ],
-            [
-                'user3',
-                'user3@user3.com',
-                'user3',
-                'TRUE',
-                ['ROLE_USER'],
-            ],
-        ];
+    $users = [
+      [
+        'admin',
+        'admin@admin.com',
+        'admin',
+        'TRUE',
+        ['ROLE_SUPER_ADMIN', 'ROLE_USER'],
+        'fixture_images/admin.png',
+      ],
+      [
+        'moder1',
+        'moder1@moder.com',
+        'moder1',
+        'TRUE',
+        ['ROLE_MODERATOR', 'ROLE_USER'],
+        'fixture_images/moder1.png',
+      ],
+      [
+        'moder2',
+        'moder2@moder.com',
+        'moder2',
+        'TRUE',
+        ['ROLE_MODERATOR', 'ROLE_USER'],
+        'fixture_images/moder2.jpg',
+      ],
+      [
+        'user1',
+        'user1@user1.com',
+        'user1',
+        'TRUE',
+        ['ROLE_USER'],
+        'fixture_images/user1.jpg',
+      ],
+      [
+        'user2',
+        'user2@user2.com',
+        'user2',
+        'TRUE',
+        ['ROLE_USER'],
+        'fixture_images/user2.png',
+      ],
+      [
+        'user3',
+        'user3@user3.com',
+        'user3',
+        'TRUE',
+        ['ROLE_USER'],
+        'fixture_images/user3.png',
+      ],
+    ];
 
-        foreach ($users as list($userName, $userEmail, $userPassword, $userEnabled, $userRole, )) {
-            $user = new User();
-            $user->setUsername($userName);
-            $user->setEmail($userEmail);
-            $user->setPlainPassword($userPassword);
-            $user->setEnabled($userEnabled);
+    foreach ($users as list($userName, $userEmail, $userPassword, $userEnabled, $userRole, $userImg)) {
+      $user = new User();
+      $user->setUsername($userName);
+      $user->setEmail($userEmail);
+      $user->setPlainPassword($userPassword);
+      $user->setEnabled($userEnabled);
+      $user->setImg($userImg);
 
-            /**@var User $setTags */
-            foreach ($userRole as $userRoles) {
-                $user->addRole($userRoles);
-            }
+      /**@var User $setTags */
+      foreach ($userRole as $userRoles) {
+        $user->addRole($userRoles);
+      }
 
-            $user->setImg();
+      $manager->persist($user);
+      $manager->flush();
 
-            $manager->persist($user);
-            $manager->flush();
-
-            $this->addReference($userName, $user);
-        }
+      $this->addReference($userName, $user);
     }
+  }
 
-    public function getOrder()
-    {
-        return 3;
-    }
+  public function getOrder() {
+    return 3;
+  }
 }
