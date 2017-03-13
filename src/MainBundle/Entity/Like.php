9555cd32a2600bb3ccde28db88e2ed9cc80e7660
MaximStrutinskiy <max.strutinskiy@gmail.com>
2017-03-13 15:56:56 +0200

    Revert "Change Like - Entity"
    This reverts commit d0807089d16237ef56affc1c80909a0664021065.

--- src/MainBundle/Entity/Like.php
+++ src/MainBundle/Entity/Like.php
@@ -12,114 +12,88 @@ use Symfony\Component\Validator\Constraints as Assert;
  * @ORM\Table(name="likes")
  * @ORM\Entity(repositoryClass="MainBundle\Repository\LikeRepository")
  **/
-class Like {
-
-  /**
-   * @ORM\Column(type="integer")
-   * @ORM\Id
-   * @ORM\GeneratedValue(strategy="AUTO")
-   */
-  protected $id;
-
-  /**
-   * Many Likes have Many User.
-   *
-   * @ORM\ManyToOne(targetEntity="User", inversedBy="like")
-   * @ORM\JoinColumn(name="user_id", referencedColumnName="id")
-   */
-  protected $user;
-
-  /**
-   * Many Likes have One Post.
-   *
-   * @ORM\ManyToOne(targetEntity="Post", inversedBy="postLike")
-   * @ORM\JoinColumn(name="post_id", referencedColumnName="id")
-   */
-  protected $post;
-
-  /**
-   * Many Likes have One Product.
-   *
-   * @ORM\ManyToOne(targetEntity="Product", inversedBy="productLike")
-   * @ORM\JoinColumn(name="product_id", referencedColumnName="id")
-   */
-  protected $product;
-
-
-  /**
-   * Get id
-   *
-   * @return integer
-   */
-  public function getId() {
-    return $this->id;
-  }
-
-  /**
-   * Set user
-   *
-   * @param \MainBundle\Entity\User $user
-   *
-   * @return Like
-   */
-  public function setUser(\MainBundle\Entity\User $user = NULL) {
-    $this->user = $user;
-
-    return $this;
-  }
-
-  /**
-   * Get user
-   *
-   * @return \MainBundle\Entity\User
-   */
-  public function getUser() {
-    return $this->user;
-  }
-
-  /**
-   * Set post
-   *
-   * @param \MainBundle\Entity\Post $post
-   *
-   * @return Like
-   */
-  public function setPost(\MainBundle\Entity\Post $post = NULL) {
-    $this->post = $post;
-
-    return $this;
-  }
-
-  /**
-   * Get post
-   *
-   * @return \MainBundle\Entity\Post
-   */
-  public function getPost() {
-    return $this->post;
-  }
+class Like
+{
 
     /**
-     * Set product
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * Many Likes have Many User.
      *
-     * @param \MainBundle\Entity\Product $product
+     * @ORM\ManyToOne(targetEntity="User", inversedBy="like")
+     * @ORM\JoinColumn(name="user_id", referencedColumnName="id")
+     */
+    protected $user;
+
+    /**
+     * Many Likes have One Post.
+     *
+     * @ORM\ManyToOne(targetEntity="Post", inversedBy="postLike")
+     * @ORM\JoinColumn(name="post_id", referencedColumnName="id")
+     */
+    protected $post;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set user
+     *
+     * @param \MainBundle\Entity\User $user
      *
      * @return Like
      */
-    public function setProduct(\MainBundle\Entity\Product $product = null)
+    public function setUser(\MainBundle\Entity\User $user = null)
     {
-        $this->product = $product;
+        $this->user = $user;
 
         return $this;
     }
 
     /**
-     * Get product
+     * Get user
      *
-     * @return \MainBundle\Entity\Product
+     * @return \MainBundle\Entity\User
      */
-    public function getProduct()
+    public function getUser()
     {
-        return $this->product;
+        return $this->user;
+    }
+
+    /**
+     * Set post
+     *
+     * @param \MainBundle\Entity\Post $post
+     *
+     * @return Like
+     */
+    public function setPost(\MainBundle\Entity\Post $post = null)
+    {
+        $this->post = $post;
+
+        return $this;
+    }
+
+    /**
+     * Get post
+     *
+     * @return \MainBundle\Entity\Post
+     */
+    public function getPost()
+    {
+        return $this->post;
     }
 }
